d433a7477205a33b7a2d19f6777443189a633f0c
NubTau <90300509+NubTau@users.noreply.github.com>
2022-05-03 19:07:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -153,9 +153,9 @@ function success(pos) {
 
   if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
     console.log('Congratulations, you reached the target');
-    alert("Yes!")
     navigator.geolocation.clearWatch(id);
   }
+  alert("Lat is :" + crd.latitude + "Long is :" + crd.longitude + "Acc is:" + crd.accuracy)
 }
 
 function error(err) {
@@ -176,3 +176,25 @@ options = {
 id = navigator.geolocation.watchPosition(success, error, options);
 
 alert(id);
+
+// var options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0
+// };
+
+// function success(pos) {
+//   var crd = pos.coords;
+
+//   console.log('Your current position is:');
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude: ${crd.longitude}`);
+//   console.log(`More or less ${crd.accuracy} meters.`);
+//   alert("Lat is :" + crd.latitude + "Long is :" + crd.longitude + "Acc is:" + crd.accuracy)
+// }
+
+// function error(err) {
+//   console.warn(`ERROR(${err.code}): ${err.message}`);
+// }
+
+// test = navigator.geolocation.getCurrentPosition(success, error, options);
